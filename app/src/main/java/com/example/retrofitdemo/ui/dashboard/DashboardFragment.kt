6439cdec2509a0f0ff0af51544e4f13899bc0ef9
Kamil Skomro <kamilskomrokamil@gmail.com>
2022-01-08 22:40:16 +0100

    file upload in dashboard fragment

--- app/src/main/java/com/example/retrofitdemo/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/retrofitdemo/ui/dashboard/DashboardFragment.kt
@@ -1,23 +1,24 @@
 package com.example.retrofitdemo.ui.dashboard
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.retrofitdemo.R
 import com.example.retrofitdemo.databinding.FragmentDashboardBinding
+import com.jaiselrahman.filepicker.activity.FilePickerActivity
+import com.jaiselrahman.filepicker.config.Configurations
 
 class DashboardFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
     private var _binding: FragmentDashboardBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -25,21 +26,44 @@ class DashboardFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
-
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
+//        val uploadBtn  = view.findViewById<Button>(R.id.upload_file_btn)
+        imagePicker()
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
+    }
+
+    private fun imagePicker() {
+        val intent = Intent(activity, FilePickerActivity::class.java)
+        intent.putExtra(FilePickerActivity.CONFIGS, Configurations.Builder()
+            .setCheckPermission(true)
+            .setShowImages(true)
+            .setShowVideos(false)
+            .setShowFiles(true)
+            .enableImageCapture(true)
+            .setMaxSelection(1)
+//            .setSuffixes("txt", "pdf", "png", "jpeg")
+            .setSkipZeroSizeFiles(true)
+            .build()
+        )
+        startActivityForResult(intent, 101)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK && data != null) {
+            val mediaFiles: Array<Parcelable>? = data.getParcelableArrayExtra(FilePickerActivity.MEDIA_FILES)
+            val path = mediaFiles!![0]
+            Toast.makeText(context, path.toString(), Toast.LENGTH_LONG)
+        } else {
+            Toast.makeText(context, "something went wrong", Toast.LENGTH_LONG)
+        }
     }
 }
